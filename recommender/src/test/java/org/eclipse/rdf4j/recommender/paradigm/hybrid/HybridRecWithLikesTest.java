a37c18c5ad5cc172ed3cc21a22d072bd81cd2366
sameedhayat <sameedhayat@gmail.com>
2017-10-11 17:42:31 +0200

    write user embeddings

--- recommender/src/test/java/org/eclipse/rdf4j/recommender/paradigm/hybrid/HybridRecWithLikesTest.java
+++ recommender/src/test/java/org/eclipse/rdf4j/recommender/paradigm/hybrid/HybridRecWithLikesTest.java
@@ -45,9 +45,9 @@ public class HybridRecWithLikesTest {
             //these are correct.
             JungGraphIndexBasedStorage graphStorage = (JungGraphIndexBasedStorage)
                     ((HybridRecommender)recRepository.getRecommender()).getDataManager().getStorage();
-            graphStorage.computeUsersEmbeddingsAverage();
-            graphStorage.writeUsersEmbeddingsAverage("user_embeddings.csv");
-//            graphStorage.mlTrainingData("ml_training_data.csv");
+            //graphStorage.computeUsersEmbeddingsAverage();
+            //graphStorage.writeUsersEmbeddingsAverage("user_embeddings.csv");
+            graphStorage.mlTrainingData("ml_training_data.csv");
             //graphStorage.printEmbeddings();
             //Use Doc2Vec Model and save the embeddings for source and targer in csv file
             /*            
