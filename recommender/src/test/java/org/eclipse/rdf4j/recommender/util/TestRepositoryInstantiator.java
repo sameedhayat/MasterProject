aa4d74adb0b270c19a04bea63880e29da5fddac4
sameedhayat <sameedhayat@gmail.com>
2018-01-18 17:19:06 +0100

    user embeddings added

--- recommender/src/test/java/org/eclipse/rdf4j/recommender/util/TestRepositoryInstantiator.java
+++ recommender/src/test/java/org/eclipse/rdf4j/recommender/util/TestRepositoryInstantiator.java
@@ -183,10 +183,10 @@ public final class TestRepositoryInstantiator {
                        //configuration.doc2VecInputPath("input_abstract.csv");
                        //configuration.rdf2VecInputPath("rdf2vec_model");
                        
-//                       configuration.loadDoc2VecEmbeddings("doc2vec_embeddings.csv");
-//                       configuration.loadRdf2VecEmbeddings("rdf2vec_embeddings.csv");
-//                       configuration.computeUserEmbeddings("user_embeddings.csv");
-//                       configuration.loadUserEmbeddings("user_embeddings.csv");
+                       configuration.loadDoc2VecEmbeddings("doc2vec_embeddings.csv");
+                       configuration.loadRdf2VecEmbeddings("rdf2vec_embeddings.csv");
+                       configuration.computeUserEmbeddings("user_embeddings.csv");
+                       configuration.loadUserEmbeddings("user_embeddings.csv");
 //                       configuration.createMlInputFile("ml_training_data.csv");
 //                       configuration.trainTreeModel("ml_training_data.csv");
                        recRepository.loadRecConfiguration(configuration);
